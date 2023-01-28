ff9e6ed0a2415c350c081b6527177cc15dcfac16
Pranav <pranav.suryadevara11@gmail.com>
2023-01-28 13:16:00 +0530

    Building our First Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-
-
-
-}
+export class AppComponent {}
