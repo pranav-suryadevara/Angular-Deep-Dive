70d7b227cb6b64872176f98766fe9358642899ef
Pranav <pranav.suryadevara11@gmail.com>
2023-01-28 16:39:15 +0530

    Angular Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ import { Course } from "./model/course";
 export class AppComponent {
   courses = COURSES;
 
+  startDate = new Date(2023, 0, 1);
+
+  title = COURSES[0].description;
+
+  price = 9.99234523456346789;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
   onCourseSelected(course: Course) {
     console.log("App Component - click event bubbled", course);
   }
--- src/db-data.ts
+++ src/db-data.ts
@@ -1,11 +1,5 @@
 export const COURSES: any = [
   {
-    id: 12,
-    description: "Git Basics By Pranav",
-    longDescription: "Basics of Git and Source Code Repositories",
-    lessonsCount: 10,
-  },
-  {
     id: 1,
     description: "Angular Core Deep Dive",
     iconUrl:
