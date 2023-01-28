cb1e1fe1ffab22613e0487f5919212339ee326ae
Pranav <pranav.suryadevara11@gmail.com>
2023-01-28 15:01:34 +0530

    @Output - Custom Events and Event Emitters

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
 
 @Component({
@@ -8,4 +8,13 @@ import { Course } from "../model/course";
 })
 export class CourseCardComponent {
   @Input() course: Course;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  onCourseView() {
+    console.log("Card Component - button clicked ...");
+
+    this.courseSelected.emit(this.course);
+  }
 }
