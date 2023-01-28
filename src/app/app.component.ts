7356d971a93724efe2d69ea3b82ebfc0d36f0071
Pranav <pranav.suryadevara11@gmail.com>
2023-01-28 17:37:28 +0530

    View Child Query Configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { COURSES } from "../db-data";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { Course } from "./model/course";
@@ -11,10 +11,18 @@ import { Course } from "./model/course";
 export class AppComponent {
   courses = COURSES;
 
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent;
+  @ViewChild("cardRef1", { read: ElementRef })
+  cardHTMLReference: CourseCardComponent;
+
+  @ViewChild("cardRef2")
+  card2: CourseCardComponent;
+
+  @ViewChild("container")
+  containerDiv: ElementRef;
 
   onCourseSelected(course: Course) {
-    console.log(this.card);
+    console.log("card1", this.cardHTMLReference);
+    console.log("card2", this.card2);
+    console.log("Container", this.containerDiv);
   }
 }
