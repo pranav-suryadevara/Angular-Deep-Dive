96acf081ce9a16f247f74f71d5237ff691515145
Pranav <pranav.suryadevara11@gmail.com>
2023-01-29 10:14:22 +0530

    ContentChild Decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseCardComponent],
+  declarations: [AppComponent, CourseCardComponent, CourseImageComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,14 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { CourseImageComponent } from "../course-image/course-image.component";
 import { Course } from "../model/course";
 
 @Component({
@@ -6,12 +16,33 @@ import { Course } from "../model/course";
   templateUrl: "./course-card.component.html",
   styleUrls: ["./course-card.component.css"],
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements AfterViewInit {
   @Input() course: Course;
   @Input() cardIndex: number;
 
   @Output()
   courseSelected = new EventEmitter<Course>();
+
+  @ContentChild("courseImage")
+  image;
+
+  @ContentChild("container")
+  container;
+
+  @ContentChild(CourseImageComponent)
+  image1: CourseImageComponent;
+
+  @ContentChild(CourseImageComponent, { read: ElementRef })
+  image2: CourseImageComponent;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    console.log(this.image);
+    console.log(this.image1);
+    console.log(this.image2);
+    console.log(this.container);
+  }
 
   onCourseView() {
     console.log("Card Component - button clicked ...");
