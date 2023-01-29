e7b5136ebde69be7a8f24d8ed344226dd3503173
Pranav <pranav.suryadevara11@gmail.com>
2023-01-29 15:42:32 +0530

    HostBinding DOM Properties and Element Attributes

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from "@angular/core";
+import { Directive, HostBinding, Input } from "@angular/core";
 
 @Directive({
   selector: "[highlighted]",
 })
 export class HighlightedDirective {
+  @Input("highlighted")
+  isHighLighted = false;
+
   constructor() {
     console.log("Directive Created");
   }
+
+  @HostBinding("class.highlighted")
+  get cssClasses() {
+    return this.isHighLighted;
+  }
+
+  @HostBinding("attr.disabled")
+  get disabled() {
+    return true;
+  }
+
+  // @HostBinding("style.border")
+  // get cssClasses() {
+  //   return "12px solid green";
+  // }
+
+  // @HostBinding("className")
+  // get cssClasses() {
+  //   return "highlighted";
+  // }
+
+  // @HostBinding("class.highlighted") // both perform the same action
+  // get cssClasses() {
+  //   return true;
+  // }
 }
