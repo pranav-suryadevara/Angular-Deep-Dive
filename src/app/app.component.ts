e9bbb43d52af4515ccb5c56ff198e771acee1158
Pranav <pranav.suryadevara11@gmail.com>
2023-02-02 12:49:39 +0530

    Custom Services - Data Modification with Http PUT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.courses$ = this.coursesService.loadCourses();
   }
+
+  save(course: Course) {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("Course Saved"));
+  }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Course } from "../model/course";
@@ -13,5 +13,11 @@ export class CoursesService {
     const params = new HttpParams().set("page", "1").set("pageSize", "10");
 
     return this.http.get<Course[]>("/api/courses", { params });
+  }
+
+  saveCourse(course: Course) {
+    const headers = new HttpHeaders().set("X-Auth", "userId");
+
+    return this.http.put(`/api/courses/${course.id}`, course, { headers });
   }
 }
